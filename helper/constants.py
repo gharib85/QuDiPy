3c041e8aff38d2fcc9ad85eeaa05d328feb9d709
bleutooth65 <51833105+bleutooth65@users.noreply.github.com>
2020-05-31 13:57:57 -0400

    Added bitmap generation function

--- helper/constants.py
+++ helper/constants.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-pi = np.pi # pi
-e = 1.602176634 * 10**-19 # electron charge (C)
-m_e = 9.1093837015 * 10**-31 # electron mass (kg)
+pi = np.pi  # pi
+e = 1.602176634 * 10**-19  # electron charge (C)
+m_e = 9.1093837015 * 10**-31  # electron mass (kg)
