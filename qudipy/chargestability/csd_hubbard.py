7014049b5ea51f9b67f3a1bde9089501584224c4
bleutooth65 <51833105+bleutooth65@users.noreply.github.com>
2020-06-23 10:19:41 -0400

    Added new file for Hubbard model CSD generation

---
+++ qudipy/chargestability/csd_hubbard.py
@@ -0,0 +1,5 @@
+'''
+File used to generate and plot charge stability diagrams from provided potentials using the Hubbrad model
+For more information about the method, see the references https://doi.org/10.1103/PhysRevB.83.235314 and https://doi.org/10.1103/PhysRevB.83.161301
+'''
+
