93b2424a9c00a38bb8560e7e315ddc81077ac58a
hmc-crab-f18 <kzhou@cs.hmc.edu>
2020-06-29 03:51:26 -0700

    finished nextnano3D tutorial

--- qudipy/potential/nextnano_3D.py
+++ qudipy/potential/nextnano_3D.py
@@ -31,9 +31,9 @@ def is_int(string):
     except ValueError:
         return False
 
-def loadFile(filename):
+def load_file(filename):
     """
-    returns a 3d array for potential.dat
+    returns a single array ordered by the coordinates for potential.dat
             a tuple of 3 element, x, y, z for coord files
     """
     data = []
@@ -78,11 +78,11 @@ def loadFile(filename):
             z = np.array(z, dtype='O')
             return x, y, z
 
-def slicePotential2D(potential, x, y, z, slice):
+def reshape_potential(potential, x, y, z, slice, option):
     """
-    input:  1d potential list, 
-            x, y ,z coordinates
-            slice is the x-y plane we want
+    input:  1d potential array, 
+            lists of x, y ,z coordinates
+            the z coordinate indicating the slice of x-y plane
     output: a 2d array of the potentials in the x-y plane
     """
     index = np.where(z==slice)[0]
@@ -90,24 +90,14 @@ def slicePotential2D(potential, x, y, z, slice):
     M = len(y)
     Q = len(z)
     pot3DArray = np.reshape(potential,(N,M,Q))
-    pot2DArray = pot3DArray[:, :, index]
+    if option == "field":
+        gradient = np.gradient(pot3DArray,x,y,z)[-1]
+        pot2DArray = gradient[:, :, index]
+    else:
+        pot2DArray = pot3DArray[:, :, index]
     return pot2DArray
-
-def slicePotential3D(potential, x, y, z):
-    """
-    input:  1d potential list, 
-            x, y ,z coordinates
-            slice is the x-y plane we want
-    output: a 2d array of the potentials in the x-y plane
-    """
-    index = np.where(z==slice)[0]
-    N = len(x)
-    M = len(y)
-    Q = len(z)
-    pot3DArray = np.reshape(potential,(N,M,Q))
-    return pot3DArray
-
-def parseVoltage(filename):
+    
+def parse_voltage(filename):
     """
     input: a string, the filename 
            an int, number of gates
@@ -124,7 +114,7 @@ def parseVoltage(filename):
             delete.append(i)
     return s
 
-def importFolder(foldername):
+def import_folder(folder):
     """
     input: a string, name of the folder where nextnano++ files are stored 
     output: a list, where each element is a list of voltages, potentials, and coordinates
@@ -134,47 +124,49 @@ def importFolder(foldername):
     for subdir, dirs, files in os.walk(folder):
         if subdir != folder and subdir[-7:] != '/output':
             counter += 1
-            voltage = parseVoltage(subdir)
+            voltage = parse_voltage(subdir)
             L.append([voltage])
         for file in files:
             filename = os.path.join(subdir, file)
             # always first .dat then .coord
             if filename[-4:] == '.dat' or filename[-6:] == '.coord':
-                L[counter-1].append(loadFile(filename))
+                L[counter-1].append(load_file(filename))
     return L
 
-def reshapePotential(potentialL, voltages):
+def group_2D_potential(potentialL, voltages, coord, slice, option):
     """
     input:  a list, where each element is a list of voltages, potentials, and coordinates
             a list of gate voltages
             a float indicating the x-y plane
     output: an n-dimensial potential file, where n = number of gates + 2
     """
-
+    potentialL_copy = potentialL.copy()
     # loop through each combination of gate voltages
-    for i in potentialL:
-        # slice an x-y plane of the potentials
-        potential3D = slicePotential3D(i[1], i[2][0], i[2][1], i[2][2])
-        i[1] = potential3D
+    for i in potentialL_copy:
+        if option == "potential":
+            # slice an x-y plane of the potentials
+            potential2D = reshape_potential(i[1], i[2][0], i[2][1], i[2][2], slice, option)
+        elif option == "field":
+            potential2D = reshape_potential(i[1], i[2][0], i[2][1], i[2][2], slice, option)
+        i[1] = potential2D
         # reverse the list of voltages for sorting purpose
         i[0].reverse()
-    potentialL.sort()
+    potentialL_copy.sort()
 
     # stack up the potential arrays in the correct order
     potential_elmt = ()
-    for i in range(len(potentialL)):
-        potential_elmt = potential_elmt + (potentialL[i][1],) 
+    for i in range(len(potentialL_copy)):
+        potential_elmt = potential_elmt + (potentialL_copy[i][1],) 
     potential_overall = np.stack(potential_elmt, axis = 0)
-    # potential_overall = np.stack((potentialL[0][1], potentialL[1][1], potentialL[2][1],potentialL[3][1],potentialL[4][1]),axis=0)
 
     # get the shape of the potential based on the number of gates and the voltages of each gate
-    # shape = ()
-    # for v in voltages:
-    #     shape = shape + (len(v),)
-    shape = (5, 103, 35, 78)       # TODO: generalize by adding coord 
+    shape = ()
+    for v in voltages:
+        if len(v) > 1:
+            shape = shape + (len(v),)
+    shape = shape+ (len(coord[0]), len(coord[1]))
     
     potential_reshaped = np.reshape(potential_overall,shape)
-    print(potential_reshaped.shape)
     return potential_reshaped
 
 
@@ -189,20 +181,27 @@ def interp(potential, voltages, coord):
     """
     x = [float(xi) for xi in coord[0]]
     y = [float(yi) for yi in coord[1]]
-    z = [float(zi) for zi in coord[2]]
-    # variables = tuple(voltages) + (x, y, z)
-    variables = (voltages[3], x, y, z)
+    variables = ()
+    for v in voltages:
+        if len(v) > 1:
+            variables = variables + (v,)
+    variables = variables+ (x,y)
     interpolating_func = RegularGridInterpolator(variables, potential)
     return interpolating_func
 
 
 ########## Tests ##########
 
-folder = 'nextnanoSims_Small'
+# folder = 'nextnanoSims_Small'
 
-potentialL = importFolder(folder)
-voltages = [V1, V2, V3, V4, V5]
-coord = potentialL[0][2]
-potentialND = reshapePotential(potentialL, voltages)
-out = interp(potentialND, voltages, coord)
-print(out([0.2,1.0,1.0,-1.0]))
+# potentialL = import_folder(folder)
+# voltages = [V1, V2, V3, V4, V5]
+# coord = potentialL[0][2]
+# potentialND = group_2D_potential(potentialL, voltages, coord, -1, "potential")
+# out = interp(potentialND, voltages, coord)
+# print(out([0.2,1.0,1.0]))
+
+# potentialL = import_folder(folder)
+# fieldND = group_2D_potential(potentialL, voltages, coord, -1, "field")
+# out2 = interp(fieldND, voltages, coord)
+# print(out2([0.2,1.0,1.0]))
