57f2ec7802d7edc607f9f4ad6b80abe2c523f871
azharuddinmoha <71407476+azharuddinmoha@users.noreply.github.com>
2020-12-09 22:02:11 -0500

    Update Test_2D.py

--- Test_2D.py
+++ Test_2D.py
@@ -16,6 +16,11 @@ import matplotlib.pyplot as plt
 from scipy.integrate import cumtrapz
 consts = qd.Constants("Si/SiO2")
 
+arr = np.array([[1,2],[3,4]])
+arr[:,1] =  arr[:,1] + 10
+print(arr)
+
+
 # First define the x(y)-coordinates
 x = np.linspace(-60,60,201)*1E-9
 y = np.linspace(-60,60,201)*1E-9
@@ -49,15 +54,20 @@ ax.set(xlabel='x-coords [nm]',ylabel='y-coords [nm]')
 plt.show()
 
 # Trying to integrate the groundstate first the x direction then the y direction 
-integral = cumtrapz([cumtrapz(probability_x, x,initial=0) for probability_x in probability],y,initial=0) # this method is wrong !fix!
-#print(integral.shape)
+#integral = cumtrapz([cumtrapz(probability_x, x,initial=0) for probability_x in probability],y,initial=0) # this method is wrong !fix!
+integral = np.array([cumtrapz(probability_x, x,initial=0) for probability_x in probability])
+n = cumtrapz(integral[:,200],y,initial=0) 
+plt.plot(x,n)
+plt.show()
 
+""""
 # Plot the integral of the groundstate wf in 2D
 fig, ax = plt.subplots()
 ax.imshow(integral,cmap='viridis',extent=[gparams.x.min()/1E-9, gparams.x.max()/1E-9,gparams.y.min()/1E-9, gparams.y.max()/1E-9])
 ax.set_title("State: integral of ground state ")
 ax.set(xlabel='x-coords [nm]',ylabel='y-coords [nm]')
 plt.show()
+"""
 
 # Define electric field 
 electric_x = np.linspace(0,1,201)
@@ -69,14 +79,19 @@ electric_field = X + Y
 # Define expectation value
 # The wavefunction has an empty array and I used the dot and transpose to "properly" multiply the arrays 
 expectation_intergrand = np.dot(np.dot(np.transpose(np.squeeze(np.real(ground_state.conj()))), electric_field), np.squeeze(np.real(ground_state)))
-expectation_integral = cumtrapz([cumtrapz(expectation_y, y,initial=0) for expectation_y in expectation_intergrand ],x,initial=0) # this method is wrong !fix!
+#expectation_integral = cumtrapz([cumtrapz(expectation_y, y,initial=0) for expectation_y in expectation_intergrand ],x,initial=0) # this method is wrong !fix!
+expectation_integral = np.array([cumtrapz(expectation_x, x,initial=0) for expectation_x in expectation_intergrand])
+print(expectation_integral.shape)
+n = cumtrapz(expectation_integral[:,200],y,initial=0) 
+plt.plot(x,n)
+plt.show()
 
+"""
 # Plot expectation value in 2D
 fig, ax = plt.subplots()
 ax.imshow(expectation_integral,cmap='viridis',extent=[gparams.x.min()/1E-9, gparams.x.max()/1E-9,gparams.y.min()/1E-9, gparams.y.max()/1E-9])
 ax.set_title("State: integral of ground state ")
 ax.set(xlabel='x-coords [nm]',ylabel='y-coords [nm]')
 plt.show()
-
-
+"""
 
