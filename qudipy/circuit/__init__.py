1f81ee211b3839377118a0b41211f995af058e78
aaran <aaranyaalexander@gmail.com>
2020-12-16 16:15:35 -0500

    Merge into ControlPulse class - plot method modified to include time_int option as "adiabatic", self.ap_data used for storage

--- qudipy/circuit/__init__.py
+++ qudipy/circuit/__init__.py
@@ -5,4 +5,3 @@ Module for handling pulse generation
 from .controlPulse import ControlPulse
 from .quantumCircuit import QuantumCircuit
 from .file_parsers import *
-from .pulseGenerator import PulseGen
